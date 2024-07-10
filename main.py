9e6883d33612f64ba16f81b838674f0ff09851d1
Yury1973 <abashkinyn@gmail.com>
2024-07-10 17:09:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# 1st programm
+print(9**0.5*5)
+
+# 2nd programm
+print(9.99>9.98 or 1000!=1000.1)
+
+# 3rd programm
+print(1234%1000//10+5678%1000//10)
+
+# 4th programm
+print(13.42*100%100==int(42.13) or 42.13*100%100==int(13.42))
